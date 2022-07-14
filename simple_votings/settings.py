c5442c18d9047cad40467399719b1d53af852e07
Melyohin-AA <melyohin.aa@gmail.com>
2022-07-15 00:21:29 +0300

    'README.md' file is created; vote variant display issue is fixed

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w1vr8(kp16ip9opueuo(^n^$dtbjc)ii+fg=+i@ayv2(a7t4z5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
